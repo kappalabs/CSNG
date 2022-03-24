d283a3f264d1614fbe2f85ae29c139aa4d823acc
Vojtěch Vašek <vojtech.vasek@matfyz.cz>
2022-03-24 21:29:30 +0100

    [FEATURE] Evaluation
     - better saving
     - central MSE loss for LN

--- lgn_deconvolve/linear_regression.py
+++ lgn_deconvolve/linear_regression.py
@@ -9,13 +9,20 @@ from sklearn.linear_model import LinearRegression
 
 class LinearRegressionModel:
 
-    def __init__(self, model_name: str):
+    def __init__(self, subfolder: str):
         self.model = None
-        self.model_name = model_name
+        self.model_name = self.get_name()
+        self.model_path = os.path.join(self.get_name(), subfolder)
+        self.model_filepath = os.path.join(self.model_path, 'model.pkl')
 
         self.stimuli_shape = None
         self.response_shape = None
         self.lr_model = None
+
+    def get_name(self):
+        name = "linear_regression_model"
+
+        return name
 
     @staticmethod
     def flatten(data):
@@ -26,14 +33,15 @@ class LinearRegressionModel:
         self.stimuli_shape = stimuli.shape[-2:]
         self.response_shape = response.shape[-2:]
 
-        if not os.path.exists(self.model_name):
+        if not os.path.isfile(self.model_filepath):
             lr_model.fit(LinearRegressionModel.flatten(response), LinearRegressionModel.flatten(stimuli))
-            with open(self.model_name, 'wb') as f:
+            os.makedirs(os.path.dirname(self.model_filepath), exist_ok=True)
+            with open(self.model_filepath, 'wb') as f:
                 pickle.dump(lr_model, f)
             self.lr_model = lr_model
 
             return
-        with open(self.model_name, 'rb') as f:
+        with open(self.model_filepath, 'rb') as f:
             self.lr_model = pickle.load(f)
 
     def predict(self, response):
