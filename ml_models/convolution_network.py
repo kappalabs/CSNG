7e24eb22fe5d38be77b67139fcc439c6f08dd9f7
Vojtěch Vašek <vojtech.vasek@matfyz.cz>
2023-04-01 20:04:16 +0200

    [FEATURE] Convolutional Training
     - updates the W&b logging
     - adds MS_SSIM loss

--- ml_models/convolution_network.py
+++ ml_models/convolution_network.py
@@ -1,7 +1,5 @@
 import os
 import copy
-from collections import defaultdict
-
 import wandb
 import torch
 import kornia.losses
@@ -12,78 +10,13 @@ import torch.nn as nn
 import torch.optim as optim
 
 from typing import Tuple
-
-from lgn_deconvolve.model_evaluator import ModelEvaluator
+from collections import defaultdict
 from ml_models.model_base import ModelBase
+from ml_models.conv_models.model_v1 import NNModel
+from lgn_deconvolve.model_evaluator import ModelEvaluator
 
 
 class ConvolutionalNetworkModel(ModelBase):
-
-    class NNModel(nn.Module):
-
-        def __init__(self, stimuli_shape, response_shape, num_filters=1):
-            super().__init__()
-
-            self.stimuli_shape = stimuli_shape
-            self.response_shape = response_shape
-
-            ngf = 32
-            self.max_pool_kernel_size = 5
-            # num_filters = int(np.prod(response_shape))
-            number_inputs = int(np.prod(response_shape) // self.max_pool_kernel_size)
-            number_outputs = int(np.prod(stimuli_shape))
-            number_output_channels = 1
-
-            self.compressed_side_size = 16
-            self.compressed_channels = 8
-            self.compressed_length = self.compressed_side_size ** 2 * self.compressed_channels
-
-            self.compress = nn.Sequential(
-                nn.Linear(in_features=number_inputs, out_features=self.compressed_length, bias=False),
-                nn.BatchNorm1d(num_features=self.compressed_length),
-                nn.ReLU(True),
-            )
-
-            self.main = nn.Sequential(
-                # input is Z, going into a convolution
-                nn.ConvTranspose2d(in_channels=self.compressed_channels, out_channels=ngf * 8, kernel_size=3, stride=1,
-                                   padding=0),
-                nn.BatchNorm2d(ngf * 8),
-                nn.ReLU(True),
-                # state size. (ngf*8) x 4 x 4
-                nn.ConvTranspose2d(ngf * 8, ngf * 4, 3, 2, 1),
-                nn.BatchNorm2d(ngf * 4),
-                nn.ReLU(True),
-                # state size. (ngf*4) x 8 x 8
-                nn.ConvTranspose2d(ngf * 4, ngf * 2, 3, 2, 1),
-                nn.BatchNorm2d(ngf * 2),
-                nn.ReLU(True),
-                # state size. (ngf*2) x 16 x 16
-                nn.ConvTranspose2d(ngf * 2, ngf, 3, 2, 1),
-                nn.BatchNorm2d(ngf),
-                nn.ReLU(True),
-                # state size. (ngf) x 32 x 32
-                nn.ConvTranspose2d(ngf, number_output_channels, 3, 2, 1),
-                nn.Tanh()
-                # state size. (number_outputs) x 64 x 64
-            )
-
-        def forward(self, x):
-            # out_img = self.deconv(x)
-            # out_img = self.conv(out_img)
-            x = x.view(x.shape[0], -1)
-            x = nn.functional.max_pool1d(x, kernel_size=self.max_pool_kernel_size, stride=self.max_pool_kernel_size)
-            # print("x.shape orig", x.shape)
-            x = self.compress(x)
-            # print("x.shape compressed", x.shape)
-            x = x.view(x.shape[0], self.compressed_channels, self.compressed_side_size, self.compressed_side_size)
-            # print("x.shape compressed resized", x.shape)
-            out_img = self.main(x)
-            # print("out_img.shape", out_img.shape)
-            out_img = nn.functional.interpolate(out_img, size=self.stimuli_shape, mode='bilinear', align_corners=False)
-            # print("out_img.shape resized", out_img.shape)
-
-            return out_img
 
     def __init__(self, checkpoint_filepath: str, device: torch.device, config: dict,
                  data_stimuli_shape: tuple, data_response_shape: tuple):
@@ -96,10 +29,12 @@ class ConvolutionalNetworkModel(ModelBase):
         self.criterion = None
         if self.model_loss == 'L1':
             self.criterion = nn.L1Loss(reduction='none')
-        elif self.model_loss == 'L2' or self.model_loss == 'MSE':
+        elif self.model_loss == 'MSE':
             self.criterion = nn.MSELoss(reduction='none')
         elif self.model_loss == 'SSIM':
             self.criterion = kornia.losses.SSIMLoss(window_size=3, reduction='none')
+        elif self.model_loss == 'MSSSIM':
+            self.criterion = kornia.losses.MS_SSIMLoss(reduction='none')
         else:
             raise Exception("Unknown loss function: " + self.model_loss)
 
@@ -126,7 +61,7 @@ class ConvolutionalNetworkModel(ModelBase):
         data = super().load_model_data()
 
         # Define the network
-        self.model = ConvolutionalNetworkModel.NNModel(self.stimuli_shape, self.response_shape)
+        self.model = NNModel(self.stimuli_shape, self.response_shape)
         self.model.to(self.device)
 
         if data is not None:
@@ -220,9 +155,11 @@ class ConvolutionalNetworkModel(ModelBase):
                 "train.best_epoch": best_epoch,
                 "train.learning_rate": scheduler.get_last_lr(),
             }, commit=False)
-            wandb.log(dict_losses, commit=True)
-            ModelEvaluator.evaluate(dataloader_val, self)
-            ModelEvaluator.log_outputs(dataloader_val, self)
+            wandb.log(dict_losses, commit=False)
+            evaluate_dict = ModelEvaluator.evaluate(dataloader_val, self, log_dict_prefix='val.')
+            wandb.log(evaluate_dict, commit=False)
+            outputs_dict = ModelEvaluator.log_outputs(dataloader_val, self, log_dict_prefix='val.')
+            wandb.log(outputs_dict, commit=True)
 
             # Adjust the learning rate
             scheduler.step()
