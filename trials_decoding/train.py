4bc88149401631532c1dd8d97bd733b1bfc08e8e
Vojtěch Vašek <vojtech.vasek@matfyz.cz>
2023-04-02 15:54:33 +0200

    [FEATURE] Just Evaluate
     - default should be False...

--- trials_decoding/train.py
+++ trials_decoding/train.py
@@ -31,7 +31,7 @@ def get_configuration():
         "model_loss": "L1",
         "model_name": 'dummy.pth',
         "clear_progress": True,
-        "evaluate": True,
+        "evaluate": False,
     }
 
     parser = argparse.ArgumentParser()
