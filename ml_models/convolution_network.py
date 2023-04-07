35e8a06ce042a93af696631aee9c5741e6740295
Vojtěch Vašek <vojtech.vasek@eyedea.cz>
2023-04-07 10:25:44 +0200

    [FEATURE] Criterion Device
     - moves criterion for CNN always to self.device

--- ml_models/convolution_network.py
+++ ml_models/convolution_network.py
@@ -35,12 +35,11 @@ class ConvolutionalNetworkModel(ModelBase):
             self.criterion = nn.MSELoss(reduction='none')
         elif self.model_loss == 'SSIM':
             self.criterion = kornia.losses.SSIMLoss(window_size=3, reduction='none')
-            self.criterion.to(self.device)
         elif self.model_loss == 'MSSSIM':
             self.criterion = kornia.losses.MS_SSIMLoss(reduction='none')
-            self.criterion.to(self.device)
         else:
             raise Exception("Unknown loss function: " + self.model_loss)
+        self.criterion.to(self.device)
 
         self.load_model()
 
